1be8636c1980face0bf10ef57cc7106d9694f1b2
Kyere Ofosu Gideon <kyereofosu2003@gmail.com>
2026-09-22 22:10:54 +0000

    Add src/02_descriptive_analysis.py

---
+++ src/02_descriptive_analysis.py
@@ -0,0 +1,7 @@
+"""Weighted and unweighted descriptive analysis for the stunting study."""
+
+def main():
+    raise NotImplementedError("Descriptive analysis will be populated after data preparation.")
+
+if __name__ == "__main__":
+    main()
