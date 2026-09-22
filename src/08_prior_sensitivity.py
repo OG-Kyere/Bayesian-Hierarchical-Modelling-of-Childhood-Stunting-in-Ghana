b1b7ba5c1b570a84473a13e60e64fa096c731103
Kyere Ofosu Gideon <kyereofosu2003@gmail.com>
2026-09-22 22:11:10 +0000

    Add src/08_prior_sensitivity.py

---
+++ src/08_prior_sensitivity.py
@@ -0,0 +1,7 @@
+"""Prior sensitivity analysis for fixed and hierarchical parameters."""
+
+def main():
+    raise NotImplementedError("Alternative prior sets will be evaluated against the primary model.")
+
+if __name__ == "__main__":
+    main()
