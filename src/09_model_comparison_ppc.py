bf88876182d3132ad9b59b9b12a7afa103a15e16
Kyere Ofosu Gideon <kyereofosu2003@gmail.com>
2026-09-22 22:11:13 +0000

    Add src/09_model_comparison_ppc.py

---
+++ src/09_model_comparison_ppc.py
@@ -0,0 +1,7 @@
+"""Posterior predictive checks and Bayesian model comparison."""
+
+def main():
+    raise NotImplementedError("Run after all harmonized models have been fitted.")
+
+if __name__ == "__main__":
+    main()
