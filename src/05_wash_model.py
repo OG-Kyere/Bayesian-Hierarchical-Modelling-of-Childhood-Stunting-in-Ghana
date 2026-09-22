8a5a62a75637d21fcafad69bdfb0b86034280aa4
Kyere Ofosu Gideon <kyereofosu2003@gmail.com>
2026-09-22 22:11:01 +0000

    Add src/05_wash_model.py

---
+++ src/05_wash_model.py
@@ -0,0 +1,7 @@
+"""Extension of the household/community model with WASH covariates."""
+
+def main():
+    raise NotImplementedError("WASH coding will be finalized from DHS source variables.")
+
+if __name__ == "__main__":
+    main()
