f0e136bbafe51565ae992ed03f6a8ad19307565f
Kyere Ofosu Gideon <kyereofosu2003@gmail.com>
2026-09-22 22:11:04 +0000

    Add src/06_maternal_education_model.py

---
+++ src/06_maternal_education_model.py
@@ -0,0 +1,7 @@
+"""Extension including maternal education."""
+
+def main():
+    raise NotImplementedError("Maternal education extension will use a harmonized analysis sample.")
+
+if __name__ == "__main__":
+    main()
